f850af5960520608a01ad17bc5ea8379cf9bfcbc
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2022-09-07 18:06:24 +0800

    Create Compare_String.sol

---
+++ Compare_String.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+library Compare_String {
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+}
